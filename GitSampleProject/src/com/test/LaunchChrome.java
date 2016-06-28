ace0070e45a47de59539fedefcbc18907178487a
gururaj <gururaj@192.168.99.1>
2016-06-28 11:59:34 +0530

    Signed-off-by: gururaj <gururaj@192.168.99.1>

---
+++ GitSampleProject/src/com/test/LaunchChrome.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+
+		System.setProperty("webdriver.chrome.driver","/Users/gururaj/Desktop/Training/jars-core/chromedriver");
+		WebDriver myTestDriver=new ChromeDriver();
+			
+		
+			myTestDriver.get("http://www.google.com");
+			
+			
+			Thread.sleep(2000);
+			myTestDriver.manage().window().maximize();
+			Thread.sleep(5000);
+			myTestDriver.close();
+			
+			
+	}
+
+}
